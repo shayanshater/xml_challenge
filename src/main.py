d521caa4c611ba30ce97aec8b9152295f41865c7
Shayan <shayanshaterpoori@outlook.com>
2025-07-17 14:28:13 +0100

    completed xml_challenge script with tests

--- src/main.py
+++ src/main.py
@@ -1,21 +1,58 @@
-from bs4 import BeautifulSoup
-import xml.etree.ElementTree as ET
-import xmlschema
+from lxml import etree
+from io import StringIO
+import os
 
-sample_valid = "data/sample_data/FSA029-Sample-Valid.xml"
-schema = "data/FSA029-Schema.xsd"
 
-def read_fsa029(filepath,schema):
-    xsd  = xmlschema.XMLSchema(schema)
+
+
+def validate_fsa029(sample_path, schema_folder):
+    #locating main schema file
+    schema_paths = os.listdir(schema_folder)
+    for path in schema_paths:
+        if "FSA029-Schema.xsd" in path:
+            fsa_schema_path = schema_folder + '/' + path
+            
+            
+    #formulating schema object       
+    with open(fsa_schema_path) as schema_file:
+        fsa_schema_doc = etree.parse(schema_file)
+    fsa_schema = etree.XMLSchema(fsa_schema_doc)
+     
+     
+    #forming a xml object   
+    with open(sample_path) as sample_file:
+        sample_text = etree.parse(sample_file)
     
-    if xsd.validate(filepath):
-        print("valid")
-    else:
-        print("not")
+    
+            
+    try:
+        #if data conforms to schema, then print a success message and return True
+        if fsa_schema.validate(sample_text):
+            print("The data and schema match")
+            return True
+        else:
+            #else statement will causes exception to raise
+            fsa_schema.assertValid(sample_text)
+    #return failure message and if validation fails
+    except etree.DocumentInvalid as invalid_error:
+        print("The data and schema do not match")
+        raise invalid_error
+    #catch other errors
+    except Exception as error:
+        print("There has been an error")
+        raise error
+        
+        
+        
     
     
+
+    
+if __name__ == "__main__":
+    sample_valid = "data/FSA029-Sample-Valid.xml"
+    sample_full = "data/FSA029-Sample-Full.xml"
+    schema_folder = "CommonTypes/v14"
     
     
-    
-    
-read_fsa029(sample_valid, schema)
+    print(validate_fsa029(sample_valid, schema_folder))
+    print(validate_fsa029(sample_full, schema_folder))
---
+++ test/test_main.py
@@ -0,0 +1,91 @@
+from src.main import validate_fsa029
+import os
+import pytest
+from lxml import etree
+
+
+
+
+@pytest.fixture()
+def schema_file_paths():
+    fsa_schema_path = "CommonTypes/v14/FSA029-Schema.xsd"
+    commontypes_schema_path = "CommonTypes/v14/CommonTypes-Schema.xsd"
+    return [fsa_schema_path, commontypes_schema_path]
+
+@pytest.fixture()
+def sample_file_paths():
+    sample_valid_path = "data/FSA029-Sample-Valid.xml"
+    sample_full_path = "data/FSA029-Sample-Full.xml"
+    
+    
+    return [sample_valid_path, sample_full_path]
+    
+    
+@pytest.mark.describe("""Class for tests that check the 
+                      integrity of the schema files.""")    
+class TestSchemaDocs:
+    @pytest.mark.it("""A test that checks if both of the FSA029 
+                    and CommontTypes schema files are in 
+                    the directory.""")
+    def test_schema_files_are_in_same_directory(self):
+        
+        schema_files = os.listdir("CommonTypes/v14")
+        
+        fsa_schema_file_name = "FSA029-Schema.xsd"
+        common_types_file_name  = "CommonTypes-Schema.xsd"
+        
+        assert fsa_schema_file_name in schema_files
+        assert common_types_file_name in schema_files
+        
+        
+    @pytest.mark.it("""A test that checks if the contents 
+                    of the schema files are unchanged after
+                    running the validation script.""")
+    def test_schema_contents_are_unchanged(self, schema_file_paths, sample_file_paths):
+        with open(schema_file_paths[0], 'r') as fsa_schema_file:
+            pre_run_fsa_schema_text = fsa_schema_file.read()
+            
+        with open(schema_file_paths[1], 'r') as commontypes_schema_file:
+            pre_run_commontypes_schema_text = commontypes_schema_file.read()
+            
+            
+        #run the script
+        schema_folder = "CommonTypes/v14"
+        validate_fsa029(sample_file_paths[0], schema_folder)
+        
+        
+        with open(schema_file_paths[0], 'r') as fsa_schema_file:
+            post_run_fsa_schema_text = fsa_schema_file.read()
+            
+        with open(schema_file_paths[1], 'r') as commontypes_schema_file:
+            post_run_commontypes_schema_text = commontypes_schema_file.read()
+            
+            
+        #assert that schema contents have not changed
+        assert pre_run_commontypes_schema_text == post_run_commontypes_schema_text
+        assert pre_run_fsa_schema_text == post_run_fsa_schema_text
+        
+            
+@pytest.mark.describe("""Class for checking that the script 
+                      passes the valid sample file""")
+class TestValidSampleData:
+    @pytest.mark.it("""A test to check if the valid 
+                    sample data conforms to the given 
+                    schema as it should""")
+    def test_valid_sample_data(self, sample_file_paths):
+        schema_folder = "CommonTypes/v14"
+        result = validate_fsa029(sample_file_paths[0], schema_folder)
+        
+        assert result == True
+    
+    
+    @pytest.mark.it("""
+                    A test to check that the full sample data raises an exception,
+                    due to it having multiple entries into the capitals section.
+                    """)
+    def test_full_sample_data_raises_exception(self, sample_file_paths):
+        schema_folder = "CommonTypes/v14"
+        
+        
+        with pytest.raises(etree.DocumentInvalid):
+            result = validate_fsa029(sample_file_paths[1], schema_folder)
